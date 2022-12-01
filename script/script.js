d3993adeaa94f0749cdfd55cf421f0c7664109cf
francescotrudu <francescotrudu84@gmail.com>
2022-12-01 12:38:55 +0100

    Upload base components

---
+++ script/script.js
@@ -0,0 +1,34 @@
+
+const playButton = document.getElementById("play");
+playButton.addEventListener("click", function() {
+
+    // add override element on minefield
+    const minefield = document.getElementById("minefield");
+    minefield.innerHTML = `<div class="box-container"></div>`;
+    
+    // Create counter 
+    for (let i = 1; i <= 100; i++) {
+        // creare elemento square
+        //aggiungo i quadrati al minefield
+        // const square = document.createElement("div");
+        const square = createNewElement();
+
+        //append element on box-container
+        const box = document.querySelector(".box-container");
+        box.appendChild(square);
+        square.innerHTML = `<p class="m-auto">${i}</p>`
+        
+        //add eventlistener onclick
+        square.addEventListener("click", function() {
+            square.classList.toggle("hold");
+        })
+    }
+})
+
+// create functions
+function createNewElement(num){
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.innerText = num;
+    return square;
+}
