1b6236fc8fb7c327cd179707a5a7d0b9c3192080
francescotrudu <francescotrudu84@gmail.com>
2022-12-11 21:35:15 +0100

    Add bomb color

--- script/script.js
+++ script/script.js
@@ -1,13 +1,21 @@
-
 const playButton = document.getElementById("play");
 playButton.addEventListener("click", function() {
+    let clicked = []; //array caselle cliccate
+    points = 0;
+    const squareDimension = 100;
+    const numOfBombs = 16;
+    const bombs = [];
+    let gameOver = false;
+    for (let i =  0; i < numOfBombs; i++) {
+        bombs.push(getRandomUniqueNumber(bombs, 1, squareDimension));
+    };
 
     // add override element on minefield
     const minefield = document.getElementById("minefield");
     minefield.innerHTML = `<div class="box-container"></div>`;
     
     // Create counter 
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= squareDimension; i++) {
         // creare elemento square
         //aggiungo i quadrati al minefield
         // const square = document.createElement("div");
@@ -20,7 +28,20 @@ playButton.addEventListener("click", function() {
         
         //add eventlistener onclick
         square.addEventListener("click", function() {
-            square.classList.toggle("hold");
+            // se clicchi su una bomba - game over
+            if (!gameOver){
+                if (bombs.includes(i)){
+                    square.classList.add("hold", "bomb");
+                    gameOver = true;
+                } else {
+                    square.classList.toggle("hold");
+                    clicked.push(i);
+                    points++;
+                }
+            } else {
+                console.log("Game Over");
+            }
+            console.log(points);
         })
     }
 })
@@ -31,4 +52,31 @@ function createNewElement(num){
     square.classList.add("square");
     square.innerText = num;
     return square;
+}
+
+function getRandomNumber(numMin, numMax) {
+    if (numMin === numMax) {
+        return numMax;
+    }
+    return Math.floor(Math.random() * (numMax - numMin + 1) + numMin);
+}
+
+
+function getRandomUniqueNumber(blackList, min, max) {
+    let isValid = false;
+    let randomNum;
+
+    //  # finché non trovo un numero valido
+    while (isValid === false) {
+        // # genero un nuovo numero randomico nell'intervallo richiesto
+        randomNum = getRandomNumber(min, max);
+
+        // # se non è un doppione
+        if (!blackList.includes(randomNum)) {
+            // # ho trovato un nuovo numero randomico nella lista
+            isValid = true;
+        }
+    }
+    // # e lo restituisco
+    return randomNum;
 }
